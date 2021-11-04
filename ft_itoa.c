5c657e15fc301519936b028a30a1c38c28c1d800
Egor-Always-AFK <52970978+Egor-Always-AFK@users.noreply.github.com>
2021-11-04 17:26:56 +0300

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,23 +11,13 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <math.h>
 
-static int	check_min(int c)
+static int check_len_int(int n)
 {
-	if (c == (-2147483648))
-		return (0);
-	return (1);
-}
-
-static size_t check_len_int(int n)
-{
-	size_t i; 
+	int i; 
 
 	i = 0;
-	if (n < 0)
-		n *= -1;
-	while (n > 10)
+	while (n)
 	{
 		i++;
 		n /= 10;
@@ -35,58 +25,30 @@ static size_t check_len_int(int n)
 	return (i);
 }
 
-static int ft_pow(int i)
-{
-	int b;
-
-	b = 1;
-	while (i > 1)
-	{
-		b *= 10;
-		i--;
-	}
-	return (b);
-}
-
-static char *copy_my_str_pls(char *c, const char *b)
-{
-	size_t	i;
-
-	i = 0;
-	while (c[i])
-	{
-		c[i] = b[i];
-		i++;
-	}
-	c[i] = '\0';
-	return(c);
-}
-
 char	*ft_itoa(int n)
 {
-	size_t	idk;
+	int	idk;
 	char	*trash_res;
-	int		trash;
-
+	int		sign_trash;
+ 
+	sign_trash = 1;
 	idk = check_len_int(n);
-	trash = ft_pow(idk - 1);
-	trash_res = malloc(sizeof(char) * (idk + 2));
+	trash_res = malloc(sizeof(char) * (idk + 1 + (n < 0)));
 	if (trash_res == NULL)
 		return (NULL);
-	if (check_min(n) == 0)
-		return (copy_my_str_pls(trash_res, "-2147483648"));
-	idk = 0;
+	trash_res[idk + 1] = '\0';
 	if (n < 0)
 	{
-		trash_res[idk++] = '-';
-		n *= -1;
+		trash_res[0] = '-';
+		sign_trash *= -1;
+		idk += 1;
 	}
-	while (trash > 0)
+	if (n == 0)
+		trash_res[0] = '0';
+	while (n)
 	{
-		trash_res[idk] = ((n / trash) + '0');
-		idk++;
-		trash /= 10;
+		trash_res[--idk] = ((n % 10) * sign_trash) + '0';	
+		n /= 10;
 	}
-	trash_res[idk] = '\0';
 	return (trash_res);
 }
